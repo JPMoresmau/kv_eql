967c320f4a0c9da99c5095fa402974e31085e61c
JP Moresmau <jp@moresmau.fr>
2021-04-24 16:27:42 +0200

    index lookup

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use rocksdb::{ColumnFamilyDescriptor, IteratorMode, Options, DB};
+use rocksdb::{ColumnFamilyDescriptor, IteratorMode, Options, DB, ReadOptions, Direction};
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 use std::fs::{File, OpenOptions};
@@ -18,15 +18,29 @@ pub enum Operation {
     Scan {
         name: String,
     },
+    KeyLookup {
+        name: String,
+        key: Box<[u8]>,
+    },
     Extract {
         names: HashSet<String>,
         operation: Box<Operation>,
     },
+    IndexLookup {
+        name: String,
+        index_name: String,
+        values: Vec<Value>,
+        keys: Vec<String>,
+    }
 }
 
 impl Operation {
     pub fn scan<N: Into<String>>(name: N) -> Self {
         Operation::Scan { name: name.into() }
+    }
+
+    pub fn key_lookup<N: Into<String>>(name: N, key: Box<[u8]>) -> Self {
+        Operation::KeyLookup { name: name.into(),key }
     }
 
     pub fn extract(extract: &[&str], operation: Operation) -> Self {
@@ -38,6 +52,11 @@ impl Operation {
             names: hs,
             operation: Box::new(operation),
         }
+    }
+
+    pub fn index_lookup<N: Into<String>,IN: Into<String>,OT: AsRef<str>>(name: N, index_name:IN, values: Vec<Value>,
+        keys: Vec<OT>,) -> Self {
+        Operation::IndexLookup{name:name.into(), index_name:index_name.into(), values, keys:keys.iter().map(|s| String::from(s.as_ref())).collect()}
     }
 }
 
@@ -134,11 +153,7 @@ impl RocksDBEQL {
             on.iter().map(|s| String::from(s.as_ref())).collect(),
         );
 
-        let file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open(self.metadata_path.clone())?;
-        serde_json::to_writer(BufWriter::new(file), &self.metadata)?;
+        self.save_metadata()?;
 
         self.execute(Operation::scan(rec_type.as_ref()))
             .try_for_each(|(k, v)| {
@@ -149,17 +164,30 @@ impl RocksDBEQL {
         Ok(())
     }
 
-    /*fn cf_handle<'a, T: AsRef<str>>(db: &'a mut DB,rec_type: T) -> Result<&'a ColumnFamily,Error> {
-        let ref_type=rec_type.as_ref();
-        let ocf1= db.cf_handle(ref_type);
-        Ok(if ocf1.is_none(){
-            db.create_cf(ref_type, &Options::default())?;
-            let cf1=db.cf_handle(ref_type).unwrap();
-            cf1
-        } else {
-            ocf1.unwrap()
-        })
-    }*/
+    fn save_metadata(&self) -> Result<()> {
+        let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(self.metadata_path.clone())?;
+        serde_json::to_writer(BufWriter::new(file), &self.metadata)?;
+        Ok(())
+    }
+
+    pub fn delete_index<T: AsRef<str>, IT: AsRef<str>>(
+        &mut self,
+        rec_type: T,
+        idx_name: IT,
+    ) -> Result<()> {
+        if let Some(m) = self.metadata.indices.get_mut(rec_type.as_ref()){
+            m.remove(idx_name.as_ref());
+            let idx_cf = index_cf_name(rec_type.as_ref(), idx_name.as_ref());
+            self.db.drop_cf(&idx_cf)?;
+
+            self.save_metadata()?;
+        }
+        Ok(())
+    }
 
     pub fn insert<T: AsRef<str>, K: AsRef<[u8]>>(
         &mut self,
@@ -184,11 +212,14 @@ impl RocksDBEQL {
         if let Some(idxs) = self.metadata.indices.get(ref_type) {
             for (idx_name, on) in idxs.iter() {
                 let idx_cf = index_cf_name(rec_type.as_ref(), idx_name);
-                let cf1 = self.db.cf_handle(&idx_cf).unwrap();
-
-                let ix_key = index_key(on, key.as_ref(), value);
-                self.db.put_cf(cf1, ix_key, key.as_ref())?;
+                if let Some(cf1) = self.db.cf_handle(&idx_cf){
+                    let ix_key = index_key(on, key.as_ref(), value);
+                    self.db.put_cf(cf1, ix_key, key.as_ref())?;
+                }
             }
+        } else {
+            self.metadata.indices.insert(String::from(ref_type),HashMap::new());
+            self.save_metadata()?;
         }
 
         Ok(())
@@ -204,8 +235,8 @@ impl RocksDBEQL {
         if let Some(cf1) = ocf1 {
             Ok(self
                 .db
-                .get_cf(cf1, key)
-                .map(|ov| ov.map(|v| serde_json::from_slice(&v).unwrap()))?)
+                .get_cf(cf1, key)?
+                .map(|v| serde_json::from_slice(&v).unwrap()))
         } else {
             Ok(None)
         }
@@ -215,6 +246,22 @@ impl RocksDBEQL {
         let ref_type = rec_type.as_ref();
         let ocf1 = self.db.cf_handle(ref_type);
         if let Some(cf1) = ocf1 {
+            if let Some(idxs) = self.metadata.indices.get(ref_type) {
+                if !idxs.is_empty() {
+                    if let Some(value)=self
+                        .db
+                        .get_cf(cf1, key.as_ref())?
+                        .map(|v| serde_json::from_slice(&v).unwrap()){
+                        for (idx_name, on) in idxs.iter() {
+                            let idx_cf = index_cf_name(rec_type.as_ref(), idx_name);
+                            if let Some(cf) = self.db.cf_handle(&idx_cf){
+                                let ix_key = index_key(on, key.as_ref(), &value);
+                                self.db.delete_cf(cf, ix_key)?;
+                            }
+                        }
+                    }
+                }
+            }
             self.db.delete_cf(cf1, key)?;
         }
         Ok(())
@@ -234,7 +281,14 @@ impl RocksDBEQL {
                         .map(|(k, v)| (k, serde_json::from_slice::<Value>(&v).unwrap()));
                     return Box::new(it);
                 }
-            }
+            },
+            Operation::KeyLookup{name, key} => {
+                let ocf1 = self.db.cf_handle(&name);
+                if let Some(cf1) = ocf1 {
+                    let v=self.db.get_cf(cf1, &key).unwrap().map(|v| (key, serde_json::from_slice::<Value>(&v).unwrap()));
+                    return Box::new(v.into_iter());
+                }
+            },
             Operation::Extract {
                 names,
                 operation: b_op,
@@ -243,10 +297,43 @@ impl RocksDBEQL {
                     self.execute(*b_op)
                         .map(move |(k, v)| (k, extract_from_value(v, &names))),
                 );
+            },
+            Operation::IndexLookup {
+                name, index_name, values, keys
+            } => {
+                let idx_cf=index_cf_name(&name, &index_name);
+                if let Some(cf) = self.db.cf_handle(&idx_cf) {
+                    let mut v = vec![];
+                   
+                    for o in values.iter() {
+                        v.append(&mut serde_json::to_vec(o).unwrap());
+                        v.push(0);
+                    }
+                    let mut opts = ReadOptions::default();
+                    let mut u=v.clone();
+                    u.pop();
+                    u.push(1);
+                    opts.set_iterate_upper_bound(u);
+                    let mode = IteratorMode::From(&v.as_ref(), Direction::Forward);
+                    let it=self.db.iterator_cf_opt(cf, opts,mode)
+                        .map(move |(k, v)| (v,extract_from_index_key(&k, &keys)));
+                    return Box::new(it);
+                }
             }
         }
         Box::new(iter::empty::<(Box<[u8]>, Value)>())
     }
+}
+
+fn extract_from_index_key<K:AsRef<[u8]>>(k: K, keys: &[String]) -> Value {
+    let mut im:HashMap<String,Value>=HashMap::new();
+    for (part,name) in k.as_ref().split(|u| *u==0).zip(keys.iter()) {
+        if !name.is_empty() {
+            im.insert(name.clone(),serde_json::from_slice(part).unwrap());
+        }
+    }
+
+    serde_json::to_value(im).unwrap()
 }
 
 fn extract_from_value(value: Value, names: &HashSet<String>) -> Value {
@@ -275,7 +362,7 @@ fn index_key<T: AsRef<str>, K: AsRef<[u8]>>(on: &[T], key: K, value: &Value) -> 
         if let Some(v2) = value.pointer(o.as_ref()) {
             v.append(&mut serde_json::to_vec(v2).unwrap());
         } else {
-            v.push(0);
+            v.append(&mut serde_json::to_vec(&Value::Null).unwrap());
         }
         v.push(0);
     }
--- tests/rocksdb_test.rs
+++ tests/rocksdb_test.rs
@@ -7,7 +7,10 @@ use anyhow::Result;
 fn test_basic() -> Result<()>{
     let path = "test_basic.db";
     {
-        let mut meta=RocksDBEQL::open(path)?;
+        let mut eql=RocksDBEQL::open(path)?;
+        let md=&eql.metadata;
+        assert_eq!(true, md.indices.is_empty());
+
         let john = json!({
             "name": "John Doe",
             "age": 43,
@@ -17,13 +20,22 @@ fn test_basic() -> Result<()>{
             ]
         });
         
-        meta.insert("type1", b"key1", &john)?;
+        eql.insert("type1", b"key1", &john)?;
+        let md=&eql.metadata;
+        assert_eq!(true, md.indices.contains_key("type1"));
+        let m2=md.indices.get("type1").unwrap();
+        assert_eq!(true, m2.is_empty());
         
-        let ov=meta.get("type1", b"key1")?;
+        let ov=eql.get("type1", b"key1")?;
         assert_eq!(ov,Some(john));
 
-        meta.delete("type1", b"key1")?;
-        let ov=meta.get("type1", b"key1")?;
+        eql.delete("type1", b"key1")?;
+        let ov=eql.get("type1", b"key1")?;
+        assert_eq!(ov,None);
+    }
+    {
+        let mut eql=RocksDBEQL::open(path)?;
+        let ov=eql.get("type1", b"key1")?;
         assert_eq!(ov,None);
     }
     RocksDBEQL::destroy( path)?;
@@ -86,29 +98,158 @@ fn test_scan() -> Result<()>{
 }
 
 #[test]
-fn text_index_metadata() -> Result<()> {
-    let path = "text_index_metadata.db";
+fn test_lookup() -> Result<()>{
+    let path = "test_lookup.db";
+    {
+        let mut meta=RocksDBEQL::open(path)?;
+        let john = json!({
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        });
+        
+        meta.insert("type1", b"key1", &john)?;
+
+        let mary = json!({
+            "name": "Mary Doe",
+            "age": 34
+        });
+        
+        meta.insert("type1", b"key2", &mary)?;
+
+        //let v1=vec![(b"key1",&john),(b"key2",&mary)];
+        let k1: [u8;4] = [b'k',b'e',b'y',b'1'];
+        let v1:Vec<(Box<[u8]>,Value)>=meta.execute(Operation::key_lookup("type1",Box::new(k1))).collect();
+        assert_eq!(1,v1.len());
+        assert_eq!(*b"key1",*v1[0].0);
+        assert_eq!(john,v1[0].1);
+      
+        let mary2 = json!({
+            "name": "Mary Doe",
+        });
+
+        let k2: [u8;4] = [b'k',b'e',b'y',b'2'];
+        let v1:Vec<(Box<[u8]>,Value)>=meta.execute(Operation::extract(&["name","phones"],Operation::key_lookup("type1",Box::new(k2)))).collect();
+        assert_eq!(1,v1.len());
+        assert_eq!(*b"key2",*v1[0].0);
+        assert_eq!(mary2,v1[0].1);
+
+    }
+    RocksDBEQL::destroy( path)?;
+    Ok(())
+}
+
+#[test]
+fn test_index_metadata() -> Result<()> {
+    let path = "test_index_metadata.db";
     {
         let mut eql=RocksDBEQL::open(path)?;
-        eql.add_index("type1", "idx1", vec!["name"])?;
-        let md=eql.metadata;
+        eql.add_index("type1", "idx1", vec!["/name"])?;
+        let md=&eql.metadata;
         assert_eq!(true, md.indices.contains_key("type1"));
         let m2=md.indices.get("type1").unwrap();
         assert_eq!(true, m2.contains_key("idx1"));
         let on=m2.get("idx1").unwrap();
         assert_eq!(1,on.len());
-        assert_eq!("name",on[0]);
+        assert_eq!("/name",on[0]);
+    }
+    {
+        let mut eql=RocksDBEQL::open(path)?;
+        let md=&eql.metadata;
+        assert_eq!(true, md.indices.contains_key("type1"));
+        let m2=md.indices.get("type1").unwrap();
+        assert_eq!(true, m2.contains_key("idx1"));
+        let on=m2.get("idx1").unwrap();
+        assert_eq!(1,on.len());
+        assert_eq!("/name",on[0]);
+        eql.delete_index("type1", "idx1")?;
+        let md=&eql.metadata;
+        assert_eq!(true, md.indices.contains_key("type1"));
+        let m2=md.indices.get("type1").unwrap();
+        assert_eq!(true, m2.is_empty());
     }
     {
         let eql=RocksDBEQL::open(path)?;
         let md=eql.metadata;
         assert_eq!(true, md.indices.contains_key("type1"));
         let m2=md.indices.get("type1").unwrap();
-        assert_eq!(true, m2.contains_key("idx1"));
-        let on=m2.get("idx1").unwrap();
-        assert_eq!(1,on.len());
-        assert_eq!("name",on[0]);
+        assert_eq!(true, m2.is_empty());
     }
-    RocksDBEQL::destroy( path)?;
+    RocksDBEQL::destroy(path)?;
+    Ok(())
+}
+
+#[test]
+fn test_index_lookup() -> Result<()> {
+    let path = "test_index_lookup.db";
+    {
+        let mut eql=RocksDBEQL::open(path)?;
+        eql.add_index("type1", "idx1", vec!["/name","/age"])?;
+        let john = json!({
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        });
+        eql.insert("type1", b"key1", &john)?;
+
+        let mary = json!({
+            "name": "Mary Doe",
+            "age": 34
+        });
+        
+        eql.insert("type1", b"key2", &mary)?;
+
+
+        let john2 = json!({
+            "nameix": "John Doe",
+            "ageix": 43,
+        });
+        let mary2 = json!({
+            "nameix": "Mary Doe",
+            "ageix": 34,
+        });
+
+        let mary3 = json!({
+            "ageix": 34,
+        });
+
+
+        let v1:Vec<(Box<[u8]>,Value)>=eql.execute(Operation::index_lookup("type1","idx1",vec![json!("John Doe")], vec!["nameix","ageix"])).collect();
+        assert_eq!(1,v1.len());
+        assert_eq!(*b"key1",*v1[0].0);
+        assert_eq!(john2,v1[0].1);
+
+        let v1:Vec<(Box<[u8]>,Value)>=eql.execute(Operation::index_lookup("type1","idx1",vec![json!("John Doe"),json!(43)], vec!["nameix","ageix"])).collect();
+        assert_eq!(1,v1.len());
+        assert_eq!(*b"key1",*v1[0].0);
+        assert_eq!(john2,v1[0].1);
+        
+        let v1:Vec<(Box<[u8]>,Value)>=eql.execute(Operation::index_lookup("type1","idx1",vec![json!("John Doe"),json!(34)], vec!["nameix","ageix"])).collect();
+        assert_eq!(0,v1.len());
+
+        let v1:Vec<(Box<[u8]>,Value)>=eql.execute(Operation::index_lookup("type1","idx1",vec![json!("Mary Doe")], vec!["nameix","ageix"])).collect();
+        assert_eq!(1,v1.len());
+        assert_eq!(*b"key2",*v1[0].0);
+        assert_eq!(mary2,v1[0].1);
+
+        let v1:Vec<(Box<[u8]>,Value)>=eql.execute(Operation::index_lookup("type1","idx1",vec![json!("Mary Doe")], vec!["","ageix"])).collect();
+        assert_eq!(1,v1.len());
+        assert_eq!(*b"key2",*v1[0].0);
+        assert_eq!(mary3,v1[0].1);
+
+        let v1:Vec<(Box<[u8]>,Value)>=eql.execute(Operation::index_lookup("type1","idx1",vec![json!("John Deer"),json!(43)], vec!["nameix","ageix"])).collect();
+        assert_eq!(0,v1.len());
+
+        eql.delete("type1", b"key1")?;
+        let v1:Vec<(Box<[u8]>,Value)>=eql.execute(Operation::index_lookup("type1","idx1",vec![json!("John Doe")], vec!["nameix","ageix"])).collect();
+        assert_eq!(0,v1.len());
+    }
+    RocksDBEQL::destroy(path)?;
     Ok(())
 }
